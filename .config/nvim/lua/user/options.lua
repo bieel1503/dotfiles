3bd0e4d746fea1f06d422cb6e231ac0cdb5553bd
bieel1503 <gcainaramos@gmail.com>
2023-04-29 10:59:26 -0300

    april

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -37,35 +37,6 @@ vim.opt.shortmess:append "c"
 
 vim.api.nvim_set_var('netrw_menu', 0)
 vim.api.nvim_set_var('netrw_banner', 0)
-vim.api.nvim_set_var('gruvbox_contrast_dark', 'hard')
-
-vim.api.nvim_set_var('sonokai_style', 'default')
-vim.api.nvim_set_var('sonokai_transparent_background', 1)
-vim.api.nvim_set_var('sonokai_better_performance', 1)
-
-require("tokyonight").setup({
-  transparent = true,
-  styles = {
-    variables = {fg = "red"}
-  }
-})
-
-require("gruvbox").setup({
-  strikethrough = false,
-  italic = false,
-  transparent_mode = true,
-})
-
-vim.cmd [[
-"set transparent background
-    augroup user_colors
-      autocmd!
-      autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE
-    augroup END
-
-    " autocmd vimenter * ++nested colorscheme gruvbox
-  colorscheme sonokai
-]]
 
 --Set indentation for some langs and wipe netrw buffers
 vim.cmd [[
