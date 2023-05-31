6b9c317f658f8fc22416ff50726e699a99bbdeb9
bieel1503 <gcainaramos@gmail.com>
2023-05-31 18:16:31 -0300

    evil

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,6 +1,8 @@
 local config = {
   cmd = {'/usr/bin/jdtls'},
   root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
-  capabilities = require("cmp_nvim_lsp").default_capabilities()
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+  filetypes = { "java" }
 }
+
 require('jdtls').start_or_attach(config)
